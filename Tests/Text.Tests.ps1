ff1561c21c85e3929deac180775d1af4b3ad29e8
Cédric Belin <cedric.belin@mc2it.com>
2026-07-08 17:06:10 +0200

    Update the tests [skip ci]

--- Tests/Text.Tests.ps1
+++ Tests/Text.Tests.ps1
@@ -9,13 +9,13 @@ Describe "Test-IsExcludedFile" {
 		"C:\Users\Cedric\.gitconfig"
 		"/usr/local/bin/pwsh"
 	) {
-		$_ | Test-IsExcludedFile | Should -BeFalse
+		$_ | Test-IsExcludedFile | Should-BeFalse
 	}
 
 	It "should return `$true if the file path contains an excluded folder" -ForEach @(
 		"C:\Projects\Cli\.git\config"
 		"/var/www/ps_modules/Pester/Pester.ps1"
 	) {
-		$_ | Test-IsExcludedFile | Should -BeTrue
+		$_ | Test-IsExcludedFile | Should-BeTrue
 	}
 }
